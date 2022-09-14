d02f359eed16ea0f4057edf698e00802f9842059
Munshif <munshif.info@gmail.com>
2022-09-14 13:55:35 +0530

    changes

--- tests/Feature/AdvertisementTest.php
+++ tests/Feature/AdvertisementTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\Repositories\AdvertisementRepository;
+
 use Tests\TestCase;
 
 class AdvertisementTest extends TestCase
@@ -13,41 +15,33 @@ class AdvertisementTest extends TestCase
      *
      * @return void
      */
-    public function test_get_all_advertisement()
-    {
-        $response = $this->withHeaders([
-            'Bearer' => 'Value',
-        ])->get('/advertisements');
 
-        $response->assertStatus(200);
+    public function test_return_all_advertiesment()
+    {
+        $advertisement = (new AdvertisementRepository())->getAllAdvertisements();
+        $this->assertInstanceOf(JsonResponse::class, $advertisement);
     }
 
-    public function test_get_advertisement_by_id()
-    {
-        $response = $this->withHeaders([
-            'Bearer' => 'Value',
-        ])->get('/advertisements/1');
 
-        $response->assertStatus(200);
+    public function test_return_advertiesment_by_id()
+    {
+        $advertisement = (new AdvertisementRepository())->getAdvertisementById(1);
+        $this->assertInstanceOf(JsonResponse::class, $advertisement);
     }
 
-    public function test_get_advertisement_by_product()
+    public function test_advertisement_by_product()
     {
-        $response = $this->withHeaders([
-            'Bearer' => 'Value',
-        ])->get('/advertisements/product/1');
-
-        $response->assertStatus(200);
+        $advertisement = (new AdvertisementRepository())->getAdvertisementByProduct(1);
+        $this->assertInstanceOf(JsonResponse::class, $advertisement);
     }
+
 
     public function test_store_advertisement()
     {
-        $response = $this->withHeaders([
-            'Bearer' => 'Value',
-        ])->post('/advertisements', ['product_id' => '1', 'title' => 'test', 'valid_until'=>'2022-02-01','discount_percentage'=>10]);
-
-        $response->assertStatus(200);
+        $advertisement = (new AdvertisementRepository())->createOrUpdateAdvertisement(['product_id' => '1', 'title' => 'test', 'valid_until'=>'2022-02-01','discount_percentage'=>10]);
+        $this->assertInstanceOf(JsonResponse::class, $advertisement);
     }
+
 
     public function test_update_advertisement()
     {
